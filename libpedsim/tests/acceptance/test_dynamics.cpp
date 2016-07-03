b53ea866f7ef8a9ae578528e476ae9d5809b7618
Christian Gloor <chgloor@silmaril.org>
2016-07-03 23:18:53 +0200

    User acceptance tests for pedestrian dynamics

---
+++ libpedsim/tests/acceptance/test_dynamics.cpp
@@ -0,0 +1,134 @@
+#include "gtest/gtest.h"
+
+#include "ped_agent.h"
+#include "ped_obstacle.h"
+#include "ped_waypoint.h"
+#include "ped_scene.h"
+#include "ped_outputwriter.h"
+
+
+class DynamicsTest : public testing::Test {
+public:
+  Ped::Tscene *pedscene;
+  Ped::Twaypoint *w1;
+  Ped::Twaypoint *w2;
+  
+  virtual void SetUp() {
+    const ::testing::TestInfo* const test_info = ::testing::UnitTest::GetInstance()->current_test_info();
+    printf("We are in test %s of test case %s.\n", test_info->name(), test_info->test_case_name());
+
+    pedscene = new Ped::Tscene(-200, -200, 400, 400);
+    w1 = new Ped::Twaypoint(0, 0, 2);
+    w2 = new Ped::Twaypoint(50, 0, 10);
+
+    Ped::OutputWriter *ow = new Ped::Frame_OutputWriter(test_info->name());
+    pedscene->setOutputWriter(ow);
+  }
+ 
+  virtual void TearDown() {
+    delete w1;
+    delete w2;
+    delete pedscene;
+  }
+};
+
+
+/// This tests if an agent stays where it was placed as long as there
+/// are no forces affecting him.
+TEST_F(DynamicsTest, moveNotIfNotAffected) {
+  Ped::Tagent *a = new Ped::Tagent();
+  a->setPosition(50, 20, 0);
+  pedscene->addAgent(a);
+
+  for (int i=0; i<10; ++i) {
+    pedscene->moveAgents(0.2);
+  }
+  
+  vector<Ped::Tagent*> all = pedscene->getAllAgents();
+  
+  ASSERT_EQ(50, all[0]->getx());
+  ASSERT_EQ(20, all[0]->gety());
+  ASSERT_EQ( 0, all[0]->getz());
+  
+  // Cleanup
+  for (Ped::Tagent* agent : pedscene->getAllAgents()) delete agent;
+}
+
+/// This tests if the agent moves towards a waypoint assigned to it. 
+TEST_F(DynamicsTest, moveTowardsWaypoint) {
+  Ped::Tagent *a = new Ped::Tagent();
+  a->setPosition(50, 0, 0);
+  a->addWaypoint(w1);
+  pedscene->addAgent(a);
+
+  for (int i=0; i<10; ++i) {
+    pedscene->moveAgents(0.2);
+  }
+  
+  vector<Ped::Tagent*> all = pedscene->getAllAgents();
+  
+  ASSERT_GT(50, all.front()->getx());
+  
+  // Cleanup
+  for (Ped::Tagent* agent : pedscene->getAllAgents()) delete agent;
+}
+
+
+/// If waypoint mode is set to BEAHVIOR_ONCE, the agent should stop
+/// once reached the last waypoint. This is only the case if the
+/// dynamics work in such a way that the agent's velocity reduced to 0
+/// after a while without any forces affecting it. Like drag, or
+/// decaying momentum.
+TEST_F(DynamicsTest, moveStopsAtLastWaypoint) {
+  Ped::Tagent *a = new Ped::Tagent();
+  a->setPosition(-50, 0, 0);
+  a->addWaypoint(w1);
+  a->setWaypointBehavior(Ped::Tagent::WaypointBehavior::BEHAVIOR_ONCE);
+  pedscene->addAgent(a);
+  
+  // Move all agents for lots of steps
+  for (int i=0; i<120; ++i) { // takes about 100 steps to reach waypoint
+    pedscene->moveAgents(0.5); // low precision for this
+  }
+  
+  vector<Ped::Tagent*> all = pedscene->getAllAgents();
+
+  // agent should be near the inner corner of the waypoint's
+  // radius. w1 is from -1 to +2 (radius 2)
+  EXPECT_NEAR(0.0, all.front()->getx(), 1.0);
+  EXPECT_NEAR(0.0, all.front()->gety(), 1.0);
+  
+  // Cleanup
+  for (Ped::Tagent* agent : pedscene->getAllAgents()) delete agent;
+}
+
+
+/// This is a numerical stability test. 10 agents are placed very
+/// close to each other spread out on the x axis. Y and Z axis
+/// positions are identical for all agents. The force pushing them
+/// appart should only affect the x axis value of their positions.
+TEST_F(DynamicsTest, moveAxisStability) {
+
+
+  for (int i = 0; i<10; i++) {
+    Ped::Tagent *a = new Ped::Tagent();
+    a->setfactorlookaheadforce(0.0); // disable that
+    a->setPosition(0.1 * i, 1.0, 0.0);
+    pedscene->addAgent(a);
+  }
+  
+  // Move all agents for some steps, with high precision (0.03)
+  for (int i=0; i<1000; ++i) {
+    pedscene->moveAgents(0.03);
+  }
+  
+  vector<Ped::Tagent*> all = pedscene->getAllAgents();
+
+  // all agents should stay on y = 1.0 and move only horizontally on the x-axis.
+  for (Ped::Tagent* agent : pedscene->getAllAgents()) {
+    EXPECT_NEAR(1.0, agent->gety(), 0.1);
+  }
+
+  // Cleanup
+  for (Ped::Tagent* agent : pedscene->getAllAgents()) delete agent;
+}
